3aa0417b57d4c2c6e0e6a389c7b039cf56a53b79
Matt Gelfand <matthew.gelfand@gmail.com>
2014-10-09 16:51:56 -0400

    nearly done

--- js/main.js
+++ js/main.js
@@ -1,10 +1,11 @@
 $(window).scroll(function() {
-		$('#list_one').each(function(){
+		$('#mag').each(function(){
 		var imagePos = $(this).offset().top;
 
 		var topOfWindow = $(window).scrollTop();
-			if (imagePos < topOfWindow+1) {
-				$(this).addClass("slideLeft");
+			if (imagePos < topOfWindow+200) {
+				console.log('line 7');
+				$(this).addClass("expandUp");
 			}
 		});
 	});
