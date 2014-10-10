a731d6891a68eaefc5770ee6d8628b3b056e2e82
MattGelfand <matthew.gelfand@gmail.com>
2014-10-10 01:47:20 -0400

    mostly optimized for mobile

--- js/main.js
+++ js/main.js
@@ -4,16 +4,45 @@ $(window).scroll(function() {
 
 		var topOfWindow = $(window).scrollTop();
 			if (imagePos < topOfWindow+200) {
-				console.log('line 7');
+				/*console.log('line 7');*/
 				$(this).addClass("expandUp");
 			}
 		});
 	});
 
 
-var s = skrollr.init({
+	/*var s = skrollr.init({
     	render: function(data) {
         //Log the current scroll position.
         console.log(data.curTop);
     		}
 		});
+
+
+function enableSkrollr() {
+	console.log('we are on desktop');
+
+	var s = skrollr.init({
+    	render: function(data) {
+        //Log the current scroll position.
+        console.log(data.curTop);
+    		}
+		});
+}
+
+function disableSkrollr() {
+	console.log('we are on mobile');
+
+	var s = skrollr.init();
+	s.destroy();
+}
+
+enquire.register("screen and (min-width: 40rem)", {
+    match : function() {
+        enableSkrollr();
+    },  
+    unmatch : function() {
+        disableSkrollr();
+    }
+});
+*/
